1cc83d9176ffaab11606e6a22cd7609bd6b890cd
lawseff <lawseff@gmail.com>
2019-11-10 18:48:36 +0300

    Add the second task solution to TaskSolver

--- src/main/java/dev/jfuture/task/TaskSolver.java
+++ src/main/java/dev/jfuture/task/TaskSolver.java
@@ -1,11 +1,14 @@
 package dev.jfuture.task;
 
 import dev.jfuture.task.document.DocumentSaver;
+import dev.jfuture.task.document.builder.DirectorsDocumentBuilder;
 import dev.jfuture.task.document.builder.DocumentBuilderHelper;
 import dev.jfuture.task.document.builder.DynamicsDocumentBuilder;
 import dev.jfuture.task.exception.DocumentException;
 import dev.jfuture.task.calculator.GenreCalculator;
 import dev.jfuture.task.calculator.GenreCalculatorImpl;
+import dev.jfuture.task.parser.imdb.ImdbMovieParser;
+import dev.jfuture.task.parser.imdb.ImdbMovieParserImpl;
 import dev.jfuture.task.parser.wikipedia.WikipediaMovieParser;
 import dev.jfuture.task.parser.wikipedia.impl.AmericanMovieParser;
 import dev.jfuture.task.parser.wikipedia.impl.ChineseMovieParser;
@@ -17,23 +20,36 @@ import org.apache.logging.log4j.Logger;
 public class TaskSolver {
 
     private static final String DYNAMICS_FILE = "dynamics.html";
+    private static final String DIRECTORS_FILE = "directors.html";
+
+    private static final DocumentBuilderHelper BUILDER_HELPER = new DocumentBuilderHelper();
+    private static final DocumentSaver DOCUMENT_SAVER = new DocumentSaver();
 
     public static void main(String[] args) {
-        WikipediaMovieParser americanMovieParser = new AmericanMovieParser();
-        WikipediaMovieParser chineseMovieParser = new ChineseMovieParser();
-        GenreCalculator genreCalculator = new GenreCalculatorImpl();
-        MovieService movieService = new MovieServiceImpl(americanMovieParser, chineseMovieParser, genreCalculator);
-        DocumentBuilderHelper helper = new DocumentBuilderHelper();
-        DynamicsDocumentBuilder dynamicsDocumentBuilder = new DynamicsDocumentBuilder(helper, movieService);
-
         try {
-            String dynamicsContent = dynamicsDocumentBuilder.getDynamicsInHtmlFormat();
-            DocumentSaver saver = new DocumentSaver();
-            saver.save(DYNAMICS_FILE, dynamicsContent);
+            solveFirstTask();
+            solveSecondTask();
         } catch (DocumentException e) {
             Logger logger = LogManager.getLogger(TaskSolver.class);
             logger.error(e.getMessage(), e);
         }
+    }
+
+    private static void solveFirstTask() throws DocumentException {
+        WikipediaMovieParser americanMovieParser = new AmericanMovieParser();
+        WikipediaMovieParser chineseMovieParser = new ChineseMovieParser();
+        GenreCalculator genreCalculator = new GenreCalculatorImpl();
+        MovieService movieService = new MovieServiceImpl(americanMovieParser, chineseMovieParser, genreCalculator);
+        DynamicsDocumentBuilder dynamicsDocumentBuilder = new DynamicsDocumentBuilder(BUILDER_HELPER, movieService);
+        String dynamicsContent = dynamicsDocumentBuilder.getDynamicsInHtmlFormat();
+        DOCUMENT_SAVER.save(DYNAMICS_FILE, dynamicsContent);
+    }
+
+    private static void solveSecondTask() throws DocumentException {
+        ImdbMovieParser parser = new ImdbMovieParserImpl();
+        DirectorsDocumentBuilder directorsDocumentBuilder = new DirectorsDocumentBuilder(BUILDER_HELPER, parser);
+        String directorsContent = directorsDocumentBuilder.getTopDirectorsInHtmlFormat();
+        DOCUMENT_SAVER.save(DIRECTORS_FILE, directorsContent);
     }
 
 }
